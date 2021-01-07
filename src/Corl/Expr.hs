2e5a87d5622cb6ab01b1520612c7efefed9fae8f
Jianbo Liu <public@jliu.net>
2021-01-07 14:06:28 -0600

    add Expr and Pretty

---
+++ src/Corl/Expr.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Corl.Expr where
+
+import Data.Hashable
+import Data.List.NonEmpty (NonEmpty)
+import Data.String
+import Data.Text (Text)
+import Data.Text.Prettyprint.Doc
+import GHC.Exts (IsList (..))
+
+newtype Name = Name Text
+  deriving newtype (Eq, IsString, Pretty, Show, Ord, Hashable)
+
+data Rec = Non | Rec deriving (Eq, Show)
+
+instance Pretty Rec where
+  pretty Rec = "letrec"
+  pretty Non = "let"
+
+data BinOp
+  = Add
+  | Sub
+  | Mul
+  deriving (Eq, Show)
+
+data UnOp = Neg deriving (Eq, Show)
+
+data Expr a
+  = Var Name -- Variables
+  | Num Int -- Numbers
+  | Constr Int Int -- Constructor tag arity
+  | Ap (Expr a) (Expr a) -- Applications
+  | Let Rec (NonEmpty (a, Expr a)) (Expr a) -- Let(rec) expressions
+  | Case (Expr a) (NonEmpty (Int, [a], Expr a)) -- Case expression
+  | Lam [a] (Expr a) -- Lambda abstractions
+  | Binary BinOp (Expr a) (Expr a) -- Binary operation
+  | Unary UnOp (Expr a) -- Unary operation
+  deriving (Eq, Show, Functor)
+
+infixl 8 $$
+
+infixl 7 $*
+
+infixl 6 $-
+
+infixl 6 $+
+
+($$), ($+), ($*), ($-) :: Expr a -> Expr a -> Expr a
+($$) = Ap
+($+) = Binary Add
+($*) = Binary Mul
+($-) = Binary Sub
+
+instance Semigroup (Expr a) where
+  (<>) = Ap
+
+isAtomic :: Expr a -> Bool
+isAtomic = \case
+  Var {} -> True
+  Num {} -> True
+  _ -> False
+
+type CoreExpr = Expr Name
+
+data Defn a = Defn Name [a] (Expr a) deriving (Eq, Show, Functor)
+
+isCAF :: Defn a -> Bool
+isCAF (Defn _ xs _) = null xs
+
+type CoreDefn = Defn Name
+
+newtype Program a = Program {unProgram :: NonEmpty (Defn a)}
+  deriving newtype (Eq, Show, Semigroup)
+  deriving stock (Functor)
+
+instance IsList (Program a) where
+  type Item (Program a) = Defn a
+  fromList = Program . fromList
+  toList = toList . unProgram
+
+type CoreProgram = Program Name
+
+preludeDefs :: CoreProgram
+preludeDefs =
+  [ Defn "I" ["x"] (Var "x"),
+    Defn "K" ["y", "z"] (Var "y"),
+    Defn "K1" ["y", "z"] (Var "z"),
+    Defn "S" ["a", "b", "c"] ((Var "a" <> Var "c") <> (Var "b" <> Var "c")),
+    Defn "compose" ["f", "g", "x"] (Var "f" <> (Var "g" <> Var "x")),
+    Defn "twice" ["f"] ((Var "compose" <>  Var "f") <> Var "f")
+  ]
---
+++ src/Corl/Pretty.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Corl.Pretty where
+
+import Corl.Expr
+import Data.List.NonEmpty (NonEmpty, toList)
+import Data.Text.Prettyprint.Doc
+import qualified Data.Text.Prettyprint.Doc.Render.String as Pretty
+
+renderDoc :: Doc a -> String
+renderDoc = Pretty.renderString . layoutSmart defaultLayoutOptions
+
+renderShow :: Pretty a => a -> String
+renderShow = renderDoc . pretty
+
+showExpr :: CoreExpr -> String
+showExpr = renderDoc . prettyExpr
+
+showProgram :: CoreProgram -> String
+showProgram = renderDoc . prettyProgram
+
+prettyProgram :: CoreProgram -> Doc a
+prettyProgram (Program ps) = vsep . toList $ fmap prettyDefn ps
+
+prettyDefn :: CoreDefn -> Doc a
+prettyDefn (Defn n params b) = pretty n <+> hsep (pretty <$> params) <+> "=" <+> prettyExpr b
+
+data Prec a = Prec
+  { precLevel :: Maybe Int,
+    precBody :: a
+  }
+  deriving (Eq, Ord, Show)
+
+prec :: Int -> a -> Prec a
+prec = Prec . Just
+
+atom :: a -> Prec a
+atom = Prec Nothing
+
+withPrec :: Int -> Prec (Doc a) -> Doc a
+withPrec level (Prec mCurr bod)
+  | maybe False (level >) mCurr = parens bod
+  | otherwise = bod
+
+prettyRec :: Rec -> Doc a
+prettyRec = \case
+  Rec -> "letrec"
+  Non -> "let"
+
+prettyBindings :: NonEmpty (Name, CoreExpr) -> Doc a
+prettyBindings = group . align . vcat . punctuate semi . toList . fmap go
+  where
+    go (l, r) = pretty l <+> "=" <+> prettyExpr r
+
+prettyAlts :: NonEmpty (Int, [Name], CoreExpr) -> Doc a
+prettyAlts = align . vcat . punctuate semi . toList . fmap go
+  where
+    go (tag, names, bod) = pretty tag <+> hsep (pretty <$> names) <+> "->" <+> prettyExpr bod
+
+prettyExpr :: CoreExpr -> Doc a
+prettyExpr = precBody . go
+  where
+    go :: CoreExpr -> Prec (Doc a)
+    go = \case
+      Var n -> atom (pretty n)
+      Num i -> atom (pretty i)
+      Ap l r -> prec 8 (withPrec 8 (go l) <+> withPrec 9 (go r))
+      Constr t x -> atom (pretty t <> "#" <> pretty x)
+      Let r bs body -> prec 3 (prettyRec r <+> prettyBindings bs <+> "in" <+> withPrec 0 (go body))
+      Case v alts -> prec 3 ("case" <+> precBody (go v) <+> "of" <+> prettyAlts alts)
+      Lam vs bod -> prec 3 ("λ" <> hsep (pretty <$> vs) <+> "->" <+> precBody (go bod))
+      Binary Add l r -> prec 6 (withPrec 7 (go l) <+> "+" <+> withPrec 6 (go r))
+      Binary Sub l r -> prec 6 (withPrec 7 (go l) <+> "-" <+> withPrec 6 (go r))
+      Binary Mul l r -> prec 7 (withPrec 8 (go l) <+> "*" <+> withPrec 7 (go r))
+      Unary Neg it -> prec 1 ("-" <+> withPrec 3 (go it))
